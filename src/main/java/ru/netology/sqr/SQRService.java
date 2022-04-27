2f347af7456712c3089a8421795d66c23af8ca3d
Dina <maskaykinadina@gmail.com>
2022-04-27 12:06:54 +0300

    reformat code

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -5,7 +5,7 @@ public class SQRService {
     public int calculateNumber(int low, int upper) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i*i >= low && i*i <= upper) {
+            if (i * i >= low && i * i <= upper) {
                 count++;
             }
         }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -10,25 +10,26 @@ class SQRServiceTest {
     void shouldCalculateNumberRange() {
         SQRService service= new SQRService();
         int expected = 3;
-        int actual = service.calculateNumber(200,300);
+        int actual = service.calculateNumber(200, 300);
 
-        assertEquals (expected, actual);
+        assertEquals(expected, actual);
     }
+
     @Test
     void shouldCalculateNumberBelowRange() {
-        SQRService service= new SQRService();
+        SQRService service = new SQRService();
         int expected = 0;
-        int actual = service.calculateNumber(0,99);
+        int actual = service.calculateNumber(0, 99);
 
-        assertEquals (expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
     void shouldCalculateNumberAboveRange() {
-        SQRService service= new SQRService();
+        SQRService service = new SQRService();
         int expected = 90;
-        int actual = service.calculateNumber(99,10000);
+        int actual = service.calculateNumber(99, 10000);
 
-        assertEquals (expected, actual);
+        assertEquals(expected, actual);
     }
 }
