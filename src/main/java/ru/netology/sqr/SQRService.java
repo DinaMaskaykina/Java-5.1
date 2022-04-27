084fd1b1544c407b0c66c8a8d5de9a78288ab55b
Dina <maskaykinadina@gmail.com>
2022-04-27 11:55:11 +0300

    initial commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,14 @@
+package ru.netology.sqr;
+
+public class SQRService {
+
+    public int calculateNumber(int low, int upper) {
+        int count = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (i*i >= low && i*i <= upper) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SQRServiceTest {
+
+    @Test
+    void shouldCalculateNumberRange() {
+        SQRService service= new SQRService();
+        int expected = 3;
+        int actual = service.calculateNumber(200,300);
+
+        assertEquals (expected, actual);
+    }
+    @Test
+    void shouldCalculateNumberBelowRange() {
+        SQRService service= new SQRService();
+        int expected = 0;
+        int actual = service.calculateNumber(0,99);
+
+        assertEquals (expected, actual);
+    }
+
+    @Test
+    void shouldCalculateNumberAboveRange() {
+        SQRService service= new SQRService();
+        int expected = 90;
+        int actual = service.calculateNumber(99,10000);
+
+        assertEquals (expected, actual);
+    }
+}
